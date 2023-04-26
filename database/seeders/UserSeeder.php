75b5430cdb5b98998c93d7c78ee78625375f58f5
SaraG94 <sara.gherbassi.94@gmail.com>
2023-04-26 17:35:22 +0200

    FEAT seeder user

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = User::create([
+            'name' => 'Sara Gherbassi',
+            'email' => 'gherba_94@hotmail.it',
+            'password' => Hash::make('pwSicura'),
+        ]);
+
+    }
+}
